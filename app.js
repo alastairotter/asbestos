81a4844b6d7b573827055cab7b19e67be10b06b1
Alastair Otter <alastair@mediahack.co.za>
2017-04-10 17:02:35 +0200

    School lookup

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ var testApp = angular.module("testApp", []);
 
 testApp.controller("mainController", ['$scope', '$http', function($scope, $http) {
 
+  $scope.searchText  = '';
+
+  $scope.reset = function () {
+    console.log("reset");
+    $scope.searchText = '';
+  }
 
   $http.get("gp_schools.json")
 
           .success( function(data) {
+
+
 
               $scope.schools = data;
               console.log(data);
